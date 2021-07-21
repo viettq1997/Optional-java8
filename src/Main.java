62a1665a6623f66e2a23b5d8afb3056e50d2eea0
Tran Quoc Viet <vietpro23w@gmail.com>
2021-07-21 22:45:01 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Optional;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // gán giá trị cho hello. Optional.of không cho phép truyền vào giá trị null
+//        Optional<String> hello = Optional.of("hello");
+
+        Optional<String> hello = Optional.ofNullable(null);
+
+        // hello != null -> hello.isPresent() = true
+        System.out.println(hello.isPresent());
+
+        // hello == null -> hello.isEmpty() = true
+        System.out.println(hello.isEmpty());
+
+        // orElse : nếu hello == null -> hello = world
+        String orElse = hello.orElse("world orElse");
+        System.out.println(orElse);
+        // -> sout: world (if hello == null)
+
+        /**
+         * if hello == null -> thuc hien map
+         * if else -> thực hiện arrow function trong orElseGet()
+         * */
+        String orElse2 = hello
+                .map(s -> {
+                    System.out.println(s);
+                    return s;
+                })
+                .orElseGet(() -> {
+                            System.out.println(hello);
+                            return "world orElseGet";
+                        }
+                );
+        System.out.println(orElse2);
+
+        hello.ifPresent(s -> {
+            System.out.println(s);
+                });
+        hello.ifPresentOrElse(s -> {
+            System.out.println(s);
+        }, () -> {
+            System.out.println("world ifPresentOrElse");
+        });
+    }
+}
+
+
---
+++ src/Program.java
@@ -0,0 +1,37 @@
+import java.util.Optional;
+
+public class Program {
+    public static void main(String[] args) {
+        Person person = new Person("james", null);
+//        String email = person
+//                .getEmail()
+//                .map(String::toLowerCase)
+//                .orElse("Email not provided");
+//        System.out.println(email);
+
+        if (person.getEmail().isPresent()) {
+            String email = person.getEmail().get();
+            System.out.println(email.toLowerCase());
+        } else {
+            System.out.println("Email not provided");
+        }
+    }
+}
+
+class Person {
+    private final String name;
+    private final String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Optional<String> getEmail() {
+        return Optional.ofNullable(email);
+    }
+}
